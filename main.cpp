5e7c407185178e0b121f3895c755667692e9def5
Ong Jian Wei <showershower59@gmail.com>
2018-04-03 14:00:06 +0800

    Finished working with mainTwo

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
 	
 	cout << "hello world" << endl;
-	cout << "hello world 233" << endl;
+	cout << "hello world 23344" << endl;
 	cout << "hello world 3" << endl;
  	system("pause");
 	return 0;
